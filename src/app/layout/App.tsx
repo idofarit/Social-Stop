e0aea754dd40c0587d78de85637512a34b4ee0aa
idofarit <idofarit@gmail.com>
2024-10-14 15:35:11 +0530

    latest commit after ci/cd prob

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -7,7 +7,7 @@ import HomePage from "../../features/Home/HomePage";
 import ModalManager from "../common/modals/ModalManager";
 import { auth } from "../config/firebase";
 import { useAppDispatch } from "../store/store";
-import NavBar from "../../app/layout/nav/Navbar";
+import NavBar from "./nav/Navbar";
 
 function App() {
   const location = useLocation();
