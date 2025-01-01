6fedf4601ae5ef2d483df219d3175b36b62614ce
idofarit <idofarit@gmail.com>
2025-01-01 13:38:36 +0530

    ssd

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -49,7 +49,7 @@ function EventDashboard() {
 
   return (
     <Grid>
-      <GridColumn mobile={16} tablet={8} computer={16} className="grid_ten">
+      <GridColumn mobile={16} tablet={8} className="grid_ten">
         {!loadedInitial ? (
           <>
             <EventListItemPlaceholder />
@@ -70,7 +70,13 @@ function EventDashboard() {
           </>
         )}
       </GridColumn>
-      <GridColumn mobile={16} tablet={8} computer={16} className="grid_six">
+      <GridColumn
+        mobile={16}
+        tablet={8}
+        floated="right"
+        width={6}
+        className="grid_six"
+      >
         <div
           className="ui fixed top sticky stick_bar"
           style={{ top: 98, width: 405, zIndex: 1 }}
